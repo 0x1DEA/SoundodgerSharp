bfd0d19fe585f656e25c53af2a9b31f79e97ffe2
See <mrmajesticsandwich@gmail.com>
2019-10-22 08:12:05 -0500

    Fix brackets and remove random crap

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Main : MonoBehaviour {
+public class Main : MonoBehaviour
+{
     public float radius;
     public GameObject enemy;
     public GameObject ring;
@@ -13,39 +14,17 @@ public class Main : MonoBehaviour {
     public float enemyRadius;
     public Vector3 center;
 
-    void Start() {
+    void Start()
+    {
         center = gameObject.transform.position;
         SpawnEnemies();
     }
 
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.R)) {
-            int bulletIteration = 0;
-            foreach (Level.marker bullet in Level.bulletStructs) {
-                Level.bulletStructs[bulletIteration].fired = false;
-                bulletIteration++;
-            }
-            int warpIteration = 0;
-            foreach (Level.marker warp in Level.warpStructs) {
-                Level.warpStructs[warpIteration].fired = false;
-                warpIteration++;
-            }
-            Level.song.time = 0;
-            Level.lastBullet = 0;
-            Level.lastWarp = 0;
-        }
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            Level.spinRate += 1;
-        }
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            Level.spinRate -= 1;
-        }
-
+    void Update()
+    {
         iframe++;
-
-        if (iframe > 30) {
+        if (iframe > 30)
+        {
             MakeRing();
             iframe = 0;
         }
@@ -53,8 +32,10 @@ public class Main : MonoBehaviour {
         Level.checkMarkers();
     }
 
-    void SpawnEnemies() {
-        for (int i = 0; i < Level.enemies; i++) {
+    void SpawnEnemies()
+    {
+        for (int i = 0; i < Level.enemies; i++)
+        {
             float ang = i * Mathf.PI * 2f / Level.enemies;
             Vector3 newPos = new Vector3(Mathf.Cos(ang) * radius, Mathf.Sin(ang) * radius, 0);
             GameObject enemyInstance = Instantiate(enemy, newPos, Quaternion.identity);
@@ -64,7 +45,8 @@ public class Main : MonoBehaviour {
         }
     }
 
-    void MakeRing() {
+    void MakeRing()
+    {
         Instantiate(ring, new Vector3(0, 0, 0), rot);
     }
 }
